0e2485f6d9318dcaa13d4c4ed5bbdf0637a3d436
sahajsikri-glitch <sahajsikri@gmail.com>
2026-07-28 11:32:00 +0530

    understanding promises

--- college-js/script.js
+++ college-js/script.js
@@ -23,23 +23,23 @@
 // bye();
 // console.log("Task3");
 
-function helloYou(n1, n2, callback) {
-  console.log("task1");
-  callback();
-  return n1 + n2;
-}
-let a = 10;
-let b = 20;
-console.log(helloYou(a, b));
+// function helloYou(n1, n2, callback) {
+//   console.log("task1");
+//   callback();
+//   return n1 + n2;
+// }
+// let a = 10;
+// let b = 20;
+// console.log(helloYou(a, b));
 
-helloYou(a, b, hi);
+// helloYou(a, b, hi);
 
-function hi() {
-  console.log("hi");
-}
-hi();
+// function hi() {
+//   console.log("hi");
+// }
+// hi();
 
-console.log(helloYou(2, 3));
+// console.log(helloYou(2, 3));
 
 //const promiseOne = new Promise((resolve, reject) => {
 //     console.log("Promise Task 1");
@@ -59,34 +59,37 @@ console.log(helloYou(2, 3));
 // }).catch((error) => {
 //     console.log(error);
 // })
-console.log("1");
-async function test() {
-  console.log("2");
-  await console.log("3");
-  console.log("4");
-}
-test();
-console.log("5");
-t1 = tes();
-console.log("5");
+// console.log("1");
+// async function test() {
+//   console.log("2");
+//   await console.log("3");
+//   console.log("4");
+// }
+// test();
+// console.log("5");
+// t1 = tes();
+// console.log("5");
 // create promises that will print username and password using resolve
 // and if username and pass not found then it will call reject state and print ERROR
-const promise = new Promise((resolve,reject)={
-  setTimeout(function(){
-    let err = true;
-    if(!err){
-      resolve("user:ABC,password:1234");
-    }
-    else{
-      reject
+// const promise = new Promise((resolve,reject)={
+//   setTimeout(function(){
+//     let err = true;
+//     if(!err){
+//       resolve("user:ABC,password:1234");
+//     }
+//     else{
+//       reject
 
-    }
-  }
-});
+//     }
+//   }
+// });
 
 async function test() {
-  console.log("message");
+  console.log("message:1");
   // console.log("4");
-  
+  const response = await fetch("./student.json");
+  const stdn = await response.json(); //await ko use karke iska data fetch kia
+  return stdn;
+  console.log("message:2");
 }
 test();
